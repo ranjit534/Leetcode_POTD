6bebb6bf83e92ebbf411ef5e8e2d9d100e43cf2b
[Ranjit Singh] <[ranjit121220@gmail.com]>
2023-03-06 18:24:19 +0530

    2nd commit

--- 06-03-23_Q-1539.cpp
+++ 06-03-23_Q-1539.cpp
@@ -1,13 +1,13 @@
-class Solution
+class Solution // Qusetion number - 1539 Leetcode
 {
 public:
-    int findKthPositive(vector<int> &arr, int k)
+    int findKthPositive(vector<int> &arr, int k) // To find Kth missing positive integer.
     {
         int low = 0, high = arr.size();
         while (low < high)
         {
             int mid = low + (high - low) / 2;
-            if ((arr[mid] - mid) > k)
+            if ((arr[mid] - mid - 1) >= k) //(arr[i] - i - 1) gives the number of missing positive numbers till ith index.
                 high = mid;
             else
                 low = mid + 1;
